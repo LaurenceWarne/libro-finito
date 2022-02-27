a1d8951caf1f894408bdfcb1082b76c6079c17cc
lWarne <laurencewarne@gmail.com>
2022-02-27 20:43:11 +0000

    Add some caliban optimisations (+ dep bump)

--- finito/main/src/fin/Routes.scala
+++ finito/main/src/fin/Routes.scala
@@ -1,11 +1,9 @@
 package fin
 
-import caliban.interop.cats.CatsInterop
+import caliban.execution.QueryExecution
 import caliban.{CalibanError, GraphQLInterpreter, Http4sAdapter}
 import cats.data.{Kleisli, OptionT}
 import cats.effect._
-import cats.effect.std.Dispatcher
-import cats.~>
 import fs2.Stream
 import org.http4s._
 import org.http4s.server.Router
@@ -19,12 +17,13 @@ object Routes {
       interpreter: GraphQLInterpreter[Any, CalibanError],
       debug: Boolean
   )(implicit
-      runtime: zio.Runtime[Env],
-      dispatcher: Dispatcher[F]
+      runtime: zio.Runtime[Env]
   ): HttpApp[F] = {
     val serviceRoutes: HttpRoutes[F] =
-      wrapRoute[F, Env](
-        Http4sAdapter.makeHttpService[Any, CalibanError](interpreter)
+      Http4sAdapter.makeHttpServiceF[F, Any, CalibanError](
+        interpreter,
+        skipValidation = true,
+        queryExecution = QueryExecution.Sequential
       )
     val app = Router[F](
       "/version" -> Kleisli.liftF(
@@ -38,25 +37,5 @@ object Routes {
       )
     ).orNotFound
     Logger.httpApp(logHeaders = true, logBody = debug)(ResponseTiming(app))
-  }
-
-  private def wrapRoute[F[_]: Async, R](
-      route: HttpRoutes[zio.RIO[R, *]]
-  )(implicit
-      dispatcher: Dispatcher[F],
-      runtime: zio.Runtime[R]
-  ): HttpRoutes[F] = {
-    val toF: zio.RIO[R, *] ~> F   = CatsInterop.toEffectK
-    val toRIO: F ~> zio.RIO[R, *] = CatsInterop.fromEffectK
-    val to: OptionT[zio.RIO[R, *], *] ~> OptionT[F, *] =
-      new (OptionT[zio.RIO[R, *], *] ~> OptionT[F, *]) {
-        def apply[A](fa: OptionT[zio.RIO[R, *], A]): OptionT[F, A] =
-          fa.mapK(toF)
-      }
-    route
-      .mapK(to)
-      .dimap((req: Request[F]) => req.mapK(toRIO))(
-        (res: Response[zio.RIO[R, *]]) => res.mapK(toF)
-      )
   }
 }
